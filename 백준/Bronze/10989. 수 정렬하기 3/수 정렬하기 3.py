b3becbd169de52d5bbe72f7ec936089fe2b396da
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2025-12-17 14:43:40 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8380 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+count = [0] * 10001
+
+for _ in range(n):
+    num = int(input())
+    count[num] += 1
+    
+for i in range(len(count)):
+    if count[i] != 0:
+        for _ in range(count[i]):
+            print(i)

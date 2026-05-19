b05cfe0660a7addcd7f1a6309b0cd7add03efe51
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-05-19 17:12:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.65 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,25 +1,29 @@
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
-        for (String player : participant)
+        
+        // 1. 참가자 명단을 맵에 전부 담기 (인원수 +1)
+        for (String player : participant) {
             map.put(player, map.getOrDefault(player, 0) + 1);
-        for (String player : completion)
+        }
+        
+        // 2. 완주자 명단을 보며 맵에서 빼기 (인원수 -1)
+        for (String player : completion) {
             map.put(player, map.get(player) - 1);
-
-        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
-
-        while(iter.hasNext()){
-            Map.Entry<String, Integer> entry = iter.next();
-            if (entry.getValue() != 0){
-                answer = entry.getKey();
+        }
+        
+        // 3. 💡 핵심: 훨씬 쉬운 출력문 (keySet 활용)
+        // map.keySet()은 맵에 저장된 "이름"들만 모아서 한 줄로 세워줍니다.
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) { // 숫자가 0이 아닌 녀석이 범인!
+                answer = key;
                 break;
             }
         }
+        
         return answer;
     }
 }

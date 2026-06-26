accac7239bca5d1a26531102615a1c3a80c85528
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-06-26 22:24:32 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.41 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder s = new StringBuilder();
+        for (char c : my_string.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                s.append(Character.toLowerCase(c));
+            } else {
+                s.append(Character.toUpperCase(c));
+            }
+        }
+        return s.toString();
+    }
+}

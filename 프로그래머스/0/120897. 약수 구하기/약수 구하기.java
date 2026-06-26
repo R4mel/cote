7ac443544511b2d394e8d598440aa473c7f17898
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-06-26 22:28:53 +0900

    [level 0] Title: 약수 구하기, Time: 0.29 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int n) {
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                cnt++;
+        }
+
+        int[] answer = new int[cnt];
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer[count++] = i;
+            }
+        }
+        return answer;
+    }
+}

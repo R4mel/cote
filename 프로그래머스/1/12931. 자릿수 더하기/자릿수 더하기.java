f2f7773bc9fcf5345ae4b81731855661032e22c9
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-06-27 21:28:15 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (char c : String.valueOf(n).toCharArray()) {
+            answer += c - '0';
+        }
+
+        return answer;
+    }
+}

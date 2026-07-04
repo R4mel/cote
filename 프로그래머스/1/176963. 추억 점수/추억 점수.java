a4b1e042669c79e88c56a43e8654e83c5b13ff88
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-07-04 17:39:54 +0900

    [level 1] Title: 추억 점수, Time: 21.10 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        int[] answer = new int[photo.length];
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                for (String s : map.keySet()) {
+                    if (photo[i][j].equals(s)) {
+                        sum += map.get(s);
+                    }
+                }
+            }
+            answer[i] = sum;
+        }
+
+        return answer;
+    }
+}

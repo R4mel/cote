0bd1c4bfe38e823cfccd70a0b1247d075f0480c6
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-07-12 19:22:23 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.79 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase()
+                .replaceAll("[^a-z0-9._-]", "")
+                .replaceAll("\\.{2,}", ".");
+        if (answer.startsWith(".")) {
+            answer = answer.substring(1);
+        }
+        if (answer.endsWith(".")) {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+        if (answer.isEmpty()) {
+            answer = "a";
+        }
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+        }
+        if (answer.endsWith(".")) {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+        if (answer.length() <= 2) {
+            while (answer.length() < 3) {
+                answer += answer.substring(answer.length() - 1);
+            }
+        }
+
+        return answer;
+    }
+}

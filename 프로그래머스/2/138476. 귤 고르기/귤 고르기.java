ba312196085f1139358427f16a95076a2cde9225
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-04 22:05:57 +0900

    [level 2] Title: 귤 고르기, Time: 43.95 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : tangerine) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        List<Integer> counts = new ArrayList<>(map.values());
+        counts.sort(Collections.reverseOrder());
+
+        int answer = 0;
+        for (int count : counts) {
+            k -= count;
+            answer++;
+            if (k <= 0)
+                break;
+        }
+        return answer;
+    }
+}

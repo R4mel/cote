d739ee1e91d92973482640509dae06945439c678
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-06 22:32:53 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 12.55 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Deque<Integer> bridge = new ArrayDeque<>();
+        int time = 0;
+        int currentWeight = 0;
+        int index = 0;
+
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.offer(0);
+        }
+
+        while (!bridge.isEmpty()) {
+            time++;
+            currentWeight -= bridge.poll();
+
+            if (index < truck_weights.length) {
+                if (currentWeight + truck_weights[index] <= weight) {
+                    bridge.offer(truck_weights[index]);
+                    currentWeight += truck_weights[index];
+                    index++;
+                } else {
+                    bridge.offer(0);
+                }
+            }
+
+        }
+        return time;
+    }
+}

4173bf256fb0438e2820acb4a65513bfdf7bfa20
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-13 10:47:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 66.35 ms, Memory: 26 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,6 @@
 def solution(arr):
     answer = []
     for a in arr:
-        if len(answer) == 0:
-            answer.append(a)
-        elif len(answer) > 0 and answer[-1] != a:
+        if len(answer) == 0 or answer[-1] != a:
             answer.append(a)
     return answer

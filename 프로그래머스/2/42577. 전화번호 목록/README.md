4fe06bced4a55575602931f057842f0556df193b
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-13 09:49:21 +0900

    [level 2] Title: 전화번호 목록, Time: 383.58 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 28.6 MB, 시간: 72.00 ms
+메모리: 39.3 MB, 시간: 383.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 13일 09:17:09
+2026년 08월 13일 09:49:18
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,12 @@
 def solution(phone_book):
     answer = True
-    phone_book.sort()
-    
-    for i in range(1, len(phone_book)):
-        if phone_book[i].startswith(phone_book[i-1]):
-            return False
-    
+    hash_map = {}
+    for phone_number in phone_book: # 북에 있는 것을 넘버로 넣는다.
+        hash_map[phone_number] = 1 # 각 번호마다 1을 할당
+    for phone_number in phone_book: 
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                return False
     return answer

1a7df3f62c5a266e73aa21a3afa7de6220ec1286
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-13 14:48:13 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 13.19 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,15 @@
+def solution(left, right):
+    answer = 0
+    def c(a):
+        count = 0
+        for i in range(1, a + 1):
+            if a % i == 0:
+                count += 1
+        return count
+    for i in range(left, right + 1):
+        if c(i) % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+    return answer
+    

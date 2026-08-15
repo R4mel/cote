e71cba6456d3d90eec58500a6480bcd7d642f4ad
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-15 18:45:07 +0900

    [level 2] Title: 할인 행사, Time: 122.78 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,21 +1,11 @@
+from collections import Counter
 def solution(want, number, discount):
     answer = 0
     for i in range(len(discount) - 10 + 1):
         d = dict(zip(want, number))
-        tmp = discount[i:i+10]
+        sale_d = Counter(discount[i:i+10])
         
-        for item in tmp:
-            if item in d:
-                d[item] -= 1
-        
-        isZero = True
-        
-        for value in d.values():
-            if value != 0:
-                isZero = False
-                break
-                
-        if isZero:
+        if d == sale_d:
             answer += 1
             
     return answer

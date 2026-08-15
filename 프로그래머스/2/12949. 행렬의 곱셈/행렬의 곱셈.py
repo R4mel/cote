2697d061850d8eb9ffa7dac5166daab5e369e555
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-15 20:16:56 +0900

    [level 2] Title: 행렬의 곱셈, Time: 44.91 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,5 +1,13 @@
-import numpy as np
-def solution(arr1, arr2):
-    A = np.array(arr1)
-    B = np.array(arr2)
-    return (A @ B).tolist()
+def solution(A, B):
+    row_A = len(A)
+    col_A = len(A[0])
+    col_B = len(B[0])
+    result = [[0] * col_B for _ in range(row_A)]
+    
+    # 2. 3중 for 문으로 행렬 곱셈 계산
+    for i in range(row_A):          # A의 행 이동
+        for j in range(col_B):      # B의 열 이동
+            for k in range(col_A):  # A의 열(B의 행) 매칭 연산
+                result[i][j] += A[i][k] * B[k][j]
+                
+    return result

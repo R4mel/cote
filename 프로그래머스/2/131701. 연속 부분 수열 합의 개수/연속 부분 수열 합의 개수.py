0c4982899fb1cfe3e447eac9681b365cf9639df0
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-19 00:15:24 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 151.57 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,10 +1,12 @@
 def solution(elements):
-    result = set()
-    elementLen = len(elements)
-    # [7, 9, 1, 1, 4, 7, 9, 1, 1, 4]
+    answer = set()
+    n = len(elements)
     elements = elements * 2
     
-    for i in range(elementLen): # 0 ~ 4
-        for j in range(elementLen): # 0 ~ 4
-            result.add(sum(elements[j:j+i+1]))
-    return len(result)
+    for j in range(n):
+        current_sum = 0
+        for length in range(1, n+1):
+            current_sum += elements[j+length-1]
+            answer.add(current_sum)
+    
+    return len(answer)

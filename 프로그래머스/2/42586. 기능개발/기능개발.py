880d12be55648fa81ed01ed2b5975f3c883d1a1b
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-19 11:23:06 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -4,11 +4,11 @@ def solution(progresses, speeds):
     answer = []
     queue = deque()
     for p, s in zip(progresses, speeds):
-        days = math.ceil((100 - p) / s)
+        days = math.ceil((100-p) / s)
         queue.append(days)
-    
+
     while queue:
-        now = queue.popleft() # 가장 왼쪽의 값
+        now = queue.popleft()
         count = 1
         while queue and now >= queue[0]:
             queue.popleft()

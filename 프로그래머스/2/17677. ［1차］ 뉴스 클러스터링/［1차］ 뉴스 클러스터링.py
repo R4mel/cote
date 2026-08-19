983f21a89f9eb4eb398a125de25c31a3f9dfaf66
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-19 17:27:57 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.37 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,20 +1,24 @@
 from collections import Counter
+
 def solution(str1, str2):
     arr1 = []
     arr2 = []
     
-    for i in range(len(str1)-1):
+    for i in range(len(str1) - 1):
         if str1[i:i+2].isalpha():
             arr1.append(str1[i:i+2].lower())
-    for i in range(len(str2)-1):
+            
+    for i in range(len(str2) - 1):
         if str2[i:i+2].isalpha():
             arr2.append(str2[i:i+2].lower())
     
     a = Counter(arr1)
     b = Counter(arr2)
-    gyo = list((a & b).elements())
-    hap = list((a | b).elements())
     
-    if len(hap) == 0:
+    gyo = sum((a & b).values())
+    hap = sum((a | b).values())
+    
+    if hap == 0:
         return 65536
-    return int(len(gyo) / len(hap) * 65536)
+        
+    return int(gyo / hap * 65536)

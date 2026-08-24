ae02020a450e28ff83eab0a68578b62378edc83d
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-24 14:34:21 +0900

    [level 1] Title: 모의고사, Time: 1.19 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 11.7 MB, 시간: 1.36 ms
+메모리: 11.6 MB, 시간: 1.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 14일 14:23:59
+2026년 08월 24일 14:34:19
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,25 +1,21 @@
 def solution(answers):
-    a = [1, 2, 3, 4, 5]
-    b = [2, 1, 2, 3, 2, 4, 2, 5]
-    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-
-    score = [0, 0, 0]
-
+    answer = []
+    one = [1,2,3,4,5]
+    two = [2,1,2,3,2,4,2,5]
+    three =[3,3,1,1,2,2,4,4,5,5]
+    
+    score = [0,0,0]
+    
     for i in range(len(answers)):
-        if answers[i] == a[i % 5]:
+        if answers[i] == one[i%len(one)]:
             score[0] += 1
-
-        if answers[i] == b[i % 8]:
+        if answers[i] == two[i%len(two)]:
             score[1] += 1
-
-        if answers[i] == c[i % 10]:
+        if answers[i] == three[i%len(three)]:
             score[2] += 1
-
-    result = []
-    max_score = max(score)
-
-    for i in range(3):
-        if score[i] == max_score:
-            result.append(i + 1)
-
-    return result
+    
+    for i in range(len(score)):
+        if score[i] == max(score):
+            answer.append(i+1)
+    
+    return answer

4200e0bde2ad533d16a2ecd76c0258b2cd40b964
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-24 14:13:35 +0900

    [level 2] Title: 전화번호 목록, Time: 73.30 ms, Memory: 28.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,8 @@
 def solution(phone_book):
-    answer = True
-    hash_map = {}
-    for phone_number in phone_book: # 북에 있는 것을 넘버로 넣는다.
-        hash_map[phone_number] = 1 # 각 번호마다 1을 할당
-    for phone_number in phone_book: 
-        temp = ""
-        for number in phone_number:
-            temp += number
-            if temp in hash_map and temp != phone_number:
-                return False
-    return answer
+    phone_book.sort()
+    
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    
+    return True

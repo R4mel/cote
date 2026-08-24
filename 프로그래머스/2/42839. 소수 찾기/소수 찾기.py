f5eb1c9e2e4302471370677984a512b51ae55b01
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-24 14:42:50 +0900

    [level 2] Title: 소수 찾기, Time: 4.61 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,19 +1,22 @@
 from itertools import permutations
 def solution(numbers):
     answer = 0
-    res = set()
-    for i in range(1, len(numbers) + 1):
-        for x in permutations(numbers, i):
-            number = int(''.join(x))
-            res.add(number)
-    for n in res:
-        isPrime = True
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                isPrime = False
-                break
-        if isPrime and n >= 2:
+    def isPrime(n):
+        if n<2:
+            return False
+        for i in range(2, int(n ** 0.5)+1):
+            if n % i==0:
+                return False
+        return True
+    
+    prime = set()
+    for i in range(1, len(numbers)+1):
+        tmp = permutations(numbers, i)
+        for t in tmp:
+            prime.add(int(''.join(t)))
+    
+    print(prime)
+    for i in prime:
+        if isPrime(i):
             answer += 1
     return answer
-            
-                

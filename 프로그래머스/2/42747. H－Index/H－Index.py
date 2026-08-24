441096e99772caaa69866292aeb44ce49c590b37
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-24 13:46:18 +0900

    [level 2] Title: H-Index, Time: 0.10 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,13 +1,12 @@
 def solution(citations):
-    citations = sorted(citations, reverse=True)
-    # [6,5,3,1,0] citations
-    # [1,2,3,4,5] i
-    h = 0
+    citations.sort(reverse=True)
+
+    answer = 0
+
     for i in range(len(citations)):
-        if i + 1 <= citations[i]:
-            h = i + 1
+        if citations[i] >= i + 1:
+            answer = i + 1
         else:
             break
-    return h
-    
-    
+
+    return answer

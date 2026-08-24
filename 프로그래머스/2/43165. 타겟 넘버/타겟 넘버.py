b39152e88f2d36073e74391253365bf688cb4be0
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-24 18:10:14 +0900

    [level 2] Title: 타겟 넘버, Time: 115.73 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,12 @@
 def solution(numbers, target):
     answer = 0
-    def dfs(index, total):
+    def dfs(index, cur):
+        nonlocal answer
         if index == len(numbers):
-            if total == target:
-                return 1
-            else:
-                return 0
-        a = dfs(index+1, total+numbers[index])
-        b = dfs(index+1, total-numbers[index])
-        return a+b 
-    return dfs(0,0)
+            if cur == target:
+                answer += 1
+            return
+        dfs(index+1, cur+numbers[index])
+        dfs(index+1, cur-numbers[index])
+    dfs(0, 0)
+    return answer

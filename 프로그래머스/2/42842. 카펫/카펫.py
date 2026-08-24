b9da48b2b9fe0ba44a0e027de3ecfc38506b42ba
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-24 15:00:41 +0900

    [level 2] Title: 카펫, Time: 331.45 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,13 @@
 def solution(brown, yellow):
+    answer = []
     total = brown + yellow
-    # total = 12
-    # brown = 10
-    # yellow = 2 -> (width-2) * (height-2)
-    # width = 4 height = 3
+    '''
+    total = 12
+    w = 4
+    h = 3
+    yellow = 2 -> (w-2) * (h-2)
+    '''
     for h in range(3, total):
         for w in range(h, total // h+1):
-            if (w-2) * (h-2) == yellow:
-                return [w, h]
+            if (w-2)*(h-2) == yellow:
+                return [w,h]

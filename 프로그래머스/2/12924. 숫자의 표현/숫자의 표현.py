5fa2b716dea5219e16f24f866552704a3fab887f
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-25 11:12:27 +0900

    [level 2] Title: 숫자의 표현, Time: 3.83 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        summ = 0
+        for j in range(i, n+1):
+            summ += j
+            if summ == n:
+                answer +=1 
+                break
+            if summ > n:
+                break
+    
+    
+    return answer

f799687dc7329bd452474c9bf8830ce1f73339f7
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2026-08-25 10:12:42 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.82 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 13.19 ms
+메모리: 11.4 MB, 시간: 0.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 13일 14:48:10
+2026년 08월 25일 10:12:39
 
 ### 문제 설명
 
--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -2,9 +2,11 @@ def solution(left, right):
     answer = 0
     def c(a):
         count = 0
-        for i in range(1, a + 1):
+        for i in range(1, int(a **0.5) + 1):
             if a % i == 0:
-                count += 1
+                count += 2
+                if i * i == a:
+                    count-=1
         return count
     for i in range(left, right + 1):
         if c(i) % 2 == 0:
